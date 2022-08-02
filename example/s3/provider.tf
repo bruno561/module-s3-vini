2ba1dca14352dfe9185557fff2236b50f49a5a04
brnpsantos1993 <brnpsantos1993@gmail.com>
2022-08-02 17:52:44 -0300

    a

--- example/s3/provider.tf
+++ example/s3/provider.tf
@@ -2,9 +2,3 @@ provider "aws" {
   region = "us-east-1"
 }
 
-  backend "s3" {
-    bucket  = "terraform-brunolabs"
-    key     = "module-s3-vini/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
